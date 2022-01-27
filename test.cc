60fa96407e2b6a30c4f5e218dabe916b49ec0b2d
Kamen Yotov <kamen@yotov.org>
2022-01-26 23:55:51 -0500

    example 3

--- test.cc
+++ test.cc
@@ -1,11 +1,10 @@
-#include <iostream>
-
 #include <gflags/gflags.h>
+#include <glog/logging.h>
 
 DEFINE_int32(answer, 0, "what is the answer?");
 
 int main(int argc, char **argv) {
     gflags::ParseCommandLineFlags(&argc, &argv, true);
-    std::cout << "the answer is " << FLAGS_answer << std::endl;
+    LOG(INFO) <<"the answer is " << FLAGS_answer;
     return 0;
 }
